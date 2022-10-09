8e815b923b9584a5024b3124bee3bfa8d8fda59d
ArtemRe <RechembeiArtem@gmail.com>
2022-10-10 00:05:22 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,96 @@
-import PhoneBook from './PhoneBook/PhoneBook';
+import React, { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar'
+import * as API from '../components/API/Api';
+import { Container } from './App.module';
 
-export const App = () => {
-  return <PhoneBook />;
-};
+
+
+
+
+
+export class App extends Component {
+  state = {
+    query: '',
+    items: [],
+    page: 1,
+    loader: false,
+    error: null,
+    totalPages: 0,
+  }
+
+  loadImages = async (query, page) => {
+    this.setState({ loader: true });
+
+    try {
+      const data = await API.fetchImages(query, page);
+      this.setState(prevState => ({
+        items: [...prevState.items, ...data.hits],
+        
+        totalPages: data.totalHits,
+      }));
+      
+    } catch (error) {
+      this.setState({ error });
+    } finally {
+      this.setState({ isLoading: false });
+
+      window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+ componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      this.loadImages(query, page);
+    }
+  }
+
+  handleSearchSubmit = query => {
+    this.setState({
+      query,
+      items: [],
+      page: 1,
+      totalPages: 0,
+    });
+  };
+
+  onLoadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+  onOpenModal = largeImageURL => {
+    this.setState({ largeImageURL });
+  };
+
+  onCloseModal = () => {
+    this.setState({ largeImageURL: '' });
+  };
+
+  render() {
+    const { items, isLoading, largeImageURL, error, page, totalPages } =
+      this.state;
+    return (
+      <Container>
+        <Searchbar onSearch={this.handleSearchSubmit} />
+        {error && <p>Whoops, something went wrong: {error.message}</p>}
+
+        {items.length > 0 && (
+          <ImageGallery items={items} onClick={this.onOpenModal} />
+        )}
+        {isLoading && <Loader />}
+
+        {page < Math.ceil(totalPages / 12) && (
+          <Button onLoadMore={this.onLoadMore} />
+        )}
+        {largeImageURL && (
+          <Modal onClose={this.onCloseModal} largeImageURL={largeImageURL} />
+        )}
+      </Container>
+    );
+  }
+}
